ded40238dfb26ea65c4b2d6c8435371d18031414
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-13 16:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+function toInteger(n){
+  let str = n+ "";
+  if(str.indexOf(".") == -1) return n;
+  else return Number(str.slice(0, str.indexOf(".")));
+}
+
 function decimalToBinary(num) {
   //Write you code here
-  
+	let ans=[];
+	while(num > 0){
+		ans.unshift(toInteger(num%2));
+		num = toInteger(num / 2);
+	}
+	return ans.join("").toString();
 }
 
 window.decimalToBinary = decimalToBinary;
