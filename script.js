6cda73d5a736c96da1b01f6ee8f9bea6fe26963f
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-17 20:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function toInteger(n){
-  let str = n+ "";
-  if(str.indexOf(".") == -1) return n;
-  else return Number(str.slice(0, str.indexOf(".")));
-}
-
 function decimalToBinary(num) {
   //Write you code here
 	let ans=[];
 	while(num > 0){
-		ans.unshift(toInteger(num%2));
-		num = toInteger(num / 2);
+		ans.unshift(num%2);
+		num = Math.floor(num / 2);
 	}
 	return ans.join("").toString();
 }
